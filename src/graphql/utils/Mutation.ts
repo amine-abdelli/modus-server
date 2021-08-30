5019ecdc7dd40d8a46c4e80a000c2a3819b1ef18
amine-abdelli <amine.abdelli@outlook.fr>
2021-08-30 19:40:00 +0200

    feat(user): Create function formatting user name

--- src/graphql/utils/Mutation.ts
+++ src/graphql/utils/Mutation.ts
@@ -3,5 +3,6 @@ export const Mutation = `
     signup(email: String!, rawPassword: String!): AuthPayload!
     createMood(userId: String!, rate: Int!, phrase: String): AuthPayload!
     deleteUser(email: String!, password: String!): Message
+    addUserName(name: String!, id: String!): AuthPayload!
   }
 `;
