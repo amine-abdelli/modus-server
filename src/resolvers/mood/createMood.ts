91e73a674502ea27a47d270c0751c3453efcfb51
amine-abdelli <amine.abdelli@outlook.fr>
2021-10-14 22:57:45 +0200

    feat(resolvers): Delete unnecessary interface

--- src/resolvers/mood/createMood.ts
+++ src/resolvers/mood/createMood.ts
@@ -1,15 +1,9 @@
 import { Context } from "src/graphql/utils/context";
 import { oneUserById } from '../../../prisma/models/user';
-import { addOneMood } from "prisma/models/mutation";
+import { addOneMoodInterface, addOneMood } from "prisma/models/mutation";
 import { ApolloError } from "apollo-server-errors";
 
-interface addOneMoodArgs {
-  userId: string,
-  rate: number,
-  phrase: string,
-}
-
-async function createMood(parent: any, { userId, rate, phrase }: addOneMoodArgs, context: Context) {
+async function createMood(parent: any, { userId, rate, phrase }: addOneMoodInterface, context: Context) {
   try {
     console.info('Trying to add a mood');
     const user = await oneUserById({ id: userId }, context.prisma);
